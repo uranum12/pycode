816cd8d52b20fc9eaff48b7c28be4cba124ae428
uranum <113694643+uranum12@users.noreply.github.com>
2023-07-18 23:18:01 +0900

    init

---
+++ tasks.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from invoke import task
+
+
+def sources() -> str:
+    return " ".join(map(str, list(Path("src").rglob("*.py"))))
+
+
+@task
+def update(c):
+    c.run("pip-compile requirements.in")
+    c.run("pip install -r requirements.txt")
+    c.run("pip install -r dev-requirements.txt")
+
+
+@task
+def format(c):
+    c.run(f"black {sources()} tasks.py")
+    c.run(f"isort {sources()} tasks.py")
+
+
+@task
+def lint(c):
+    c.run(f"flake8 {sources()}")
+    c.run(f"mypy {sources()}")
